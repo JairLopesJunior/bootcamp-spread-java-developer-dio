0ed726e1e27a1a716e66090a7226b3439eb44461
unknown <jair.lopes@fatec.sp.gov.br>
2022-01-05 22:34:01 -0300

    Criado a classe Mentoria

--- Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-poo/src/br/com/dio/desafio/dominio/Main.java
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-poo/src/br/com/dio/desafio/dominio/Main.java
@@ -1,10 +1,28 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Curso curso = new Curso();
+		curso.setTitulo("Curso Java");
+		curso.setDescricao("Descrição do Curso");
+		curso.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Java2");
+		curso2.setDescricao("Descrição do Curso2");
+		curso2.setCargaHoraria(2);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria Java");
+		mentoria.setDescricao("Descrição da mentoria");
+		mentoria.setData(LocalDate.now());
 
+		System.out.println(curso);
+		System.out.println(curso2);
+		System.out.println(mentoria);
 	}
 
 }
