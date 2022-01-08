5fd8bf3919922a452a6a5ae98b190a2b17c10c71
unknown <jair.lopes@fatec.sp.gov.br>
2022-01-08 11:02:00 -0300

    Interligando as classes de dominio

--- Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-poo/src/br/com/dio/desafio/dominio/Main.java
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-poo/src/br/com/dio/desafio/dominio/Main.java
@@ -19,10 +19,37 @@ public class Main {
 		mentoria.setTitulo("Mentoria Java");
 		mentoria.setDescricao("Descrição da mentoria");
 		mentoria.setData(LocalDate.now());
-
-		System.out.println(curso);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("bootcampo Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java");
+		bootcamp.getConteudos().add(curso);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev dev = new Dev();
+		dev.setNome("Jair");
+		dev.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos: " + dev.getConteudosInscritos());
+		dev.progredir();
+		dev.progredir();
+		System.out.println("Conteudos Inscritos: " + dev.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos: " + dev.getConteudosConcluidos());
+		System.out.println("XP: " + dev.calcularTotalXp());
+		
+		Dev dev2 = new Dev();
+		dev2.setNome("João");
+		dev2.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos: " + dev2.getConteudosInscritos());
+		dev2.progredir();
+		System.out.println("Conteudos Inscritos: " + dev2.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos: " + dev2.getConteudosConcluidos());
+		System.out.println("XP: " + dev2.calcularTotalXp());
+		
+		
+//		System.out.println(curso);
+//		System.out.println(curso2);
+//		System.out.println(mentoria);
 	}
 
 }
